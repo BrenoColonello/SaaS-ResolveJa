7143b8fc154f85fb29368fa3b37792ab7589c071
BrenoColonello <brenosony1@gmail.com>
2026-07-05 22:56:06 -0300

    chore: setup inicial do projeto Spring Boot

---
+++ resolveja/src/main/java/com/resolveja/resolveja/ResolvejaApplication.java
@@ -0,0 +1,13 @@
+package com.resolveja.resolveja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolvejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolvejaApplication.class, args);
+	}
+
+}
---
+++ resolveja/src/test/java/com/resolveja/resolveja/ResolvejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resolveja.resolveja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolvejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
